ec0c35a3b46e89f9868bfff85c71e7798b81a233
Marcelo Alvarez <rmalvarezq@gmail.com>
2025-07-06 17:41:55 -0500

    prueba de mapa aun no funcional

--- lib/screens/user/map_driver_list_screen.dart
+++ lib/screens/user/map_driver_list_screen.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class MapDriverListScreen extends ConsumerStatefulWidget {
   const MapDriverListScreen({super.key});
@@ -14,11 +15,25 @@ class MapDriverListScreen extends ConsumerStatefulWidget {
 class _MapDriverListScreenState extends ConsumerState<MapDriverListScreen> {
   late GoogleMapController _mapController;
   final Set<Marker> _markers = {};
+  bool _permisosOtorgados = false;
 
   @override
   void initState() {
     super.initState();
-    _loadDrivers();
+    _initApp();
+  }
+
+  Future<void> _initApp() async {
+    final permisos = await Permission.location.request();
+
+    if (permisos.isGranted && mounted) {
+      setState(() {
+        _permisosOtorgados = true;
+      });
+      _loadDrivers();
+    } else {
+      await openAppSettings(); // por si están denegados
+    }
   }
 
   void _loadDrivers() {
@@ -28,6 +43,7 @@ class _MapDriverListScreenState extends ConsumerState<MapDriverListScreen> {
         .snapshots()
         .listen((snapshot) {
           final markers = <Marker>{};
+
           for (var doc in snapshot.docs) {
             final data = doc.data();
             final GeoPoint? location = data['currentLocation'];
@@ -44,8 +60,14 @@ class _MapDriverListScreenState extends ConsumerState<MapDriverListScreen> {
               );
             }
           }
-          setState(() => _markers.clear());
-          setState(() => _markers.addAll(markers));
+
+          if (mounted) {
+            setState(() {
+              _markers
+                ..clear()
+                ..addAll(markers);
+            });
+          }
         });
   }
 
@@ -53,15 +75,17 @@ class _MapDriverListScreenState extends ConsumerState<MapDriverListScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Choferes Cercanos')),
-      body: GoogleMap(
-        initialCameraPosition: const CameraPosition(
-          target: LatLng(-0.1807, -78.4678), // Quito
-          zoom: 13,
-        ),
-        onMapCreated: (controller) => _mapController = controller,
-        markers: _markers,
-        myLocationEnabled: true,
-      ),
+      body: _permisosOtorgados
+          ? GoogleMap(
+              initialCameraPosition: const CameraPosition(
+                target: LatLng(-0.1807, -78.4678), // Quito
+                zoom: 7,
+              ),
+              onMapCreated: (controller) => _mapController = controller,
+              markers: _markers,
+              myLocationEnabled: true,
+            )
+          : const Center(child: CircularProgressIndicator()),
     );
   }
 }
